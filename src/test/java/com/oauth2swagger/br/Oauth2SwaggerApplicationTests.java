5a50f0e00ce1ac2aca07b2aff6ccc5b13e3af669
João Rosa <jrrosa@live.com>
2020-01-02 21:34:02 -0400

    hello

---
+++ src/test/java/com/oauth2swagger/br/Oauth2SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2swagger.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
